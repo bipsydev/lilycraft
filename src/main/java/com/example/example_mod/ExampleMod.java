e74a460d228d7f7b7adca9bfeb1d8629a8dc1ebd
Lily-Heather Crawford (bipsydev) <lilithalicecrawford@gmail.com>
2023-08-14 23:17:48 -0500

    Initial commit - change name

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -5,14 +5,16 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class ExampleMod implements ModInitializer
+{
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("LilyCraft+");
 
 	@Override
-	public void onInitialize(ModContainer mod) {
+	public void onInitialize(ModContainer mod)
+	{
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
 	}
 }
--- src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
@@ -8,9 +8,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class TitleScreenMixin
+{
 	@Inject(method = "init", at = @At("TAIL"))
-	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	public void onInit(CallbackInfo ci)
+	{
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin! Here's some callback info: " + ci);
 	}
 }
