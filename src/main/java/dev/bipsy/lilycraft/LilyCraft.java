8db4b128278d8bbeadde09861465ccec9e956358
Lily-Heather Crawford <lilithalicecrawford@gmail.com>
2023-08-17 14:47:24 -0500

    Add Colors utility, add icon

--- src/main/java/dev/bipsy/lilycraft/LilyCraft.java
+++ src/main/java/dev/bipsy/lilycraft/LilyCraft.java
@@ -5,12 +5,16 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static dev.bipsy.utils.Colors.*;
+
 public class LilyCraft implements ModInitializer
 {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("LilyCraft+");
+	public static final Logger LOGGER = LoggerFactory.getLogger(
+		colorfmt("LilyCraft+", PURPLE_BRIGHT, GREEN_BACKGROUND)
+	);
 
 	@Override
 	public void onInitialize(ModContainer mod)
